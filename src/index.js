0358af77ee2abb2c9ba90a4da4e9b572667bdad3
Astone345 <astone345@gmail.com>
2017-09-23 18:03:58 -0500

    Ravenous app using Yelp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Componens/App/app.css';
-import App from './Components/App/App';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
